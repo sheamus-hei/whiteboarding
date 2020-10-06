170487480ed9793bc5ffe323ea6f9e13113974ea
deanjheikkila <deanjheikkila@gmail.com>
2020-10-05 18:56:25 -0700

    binary search article

--- search-and-sort/binary_search.py
+++ search-and-sort/binary_search.py
@@ -1,21 +1,21 @@
 # a binary search allows us to search for an element in a sorted array.
 # this is more efficient than a loop because the time complexity is O(logN) instead of O(N).
 
-def search2(lis, value, lo, hi):
+def search2(nums, value, lo, hi):
   if lo > hi:
     return None # not in list
   mid = (lo + hi) // 2
-  if (lis[mid] < value): # look in upper half
-    return search2(lis, value, mid + 1, hi)
-  elif (lis[mid] > value): # look in lower half
-    return search2(lis, value, lo, mid - 1)
-  else: # if value == lis[mid]
+  if (nums[mid] < value): # look in upper half
+    return search2(nums, value, mid + 1, hi)
+  elif (nums[mid] > value): # look in lower half
+    return search2(nums, value, lo, mid - 1)
+  else: # if value == nums[mid]
     return mid
 
-def binary_search(lis, value):
+def binary_search(nums, value):
   lo = 0
-  hi = len(lis) - 1
-  return search2(lis, value, lo, hi)
+  hi = len(nums) - 1
+  return search2(nums, value, lo, hi)
 
 # >>> binary_search([1,2,3], 2)
 # 1
